95795a05cbe1b8a7d5007a4d96fbcde6ed633319
Danilo Medina <141359607+dmedinaa@users.noreply.github.com>
2023-10-11 14:29:10 -0300

    Acrescentando comentário VolumedeumCone.java

--- JavaMatematica2/VolumedeumCone.java
+++ JavaMatematica2/VolumedeumCone.java
@@ -4,9 +4,9 @@ public class VolumedeumCone {
     public static void main(String[] args) {
         double volumeCone = 94; // cm³
         double alturaCone = 9; // cm
-        double raioCone = Math.sqrt(3 * volumeCone / (Math.PI * alturaCone));
+        double raioCone = Math.sqrt(3 * volumeCone / (Math.PI * alturaCone)); //colocamos a formula.
         System.out.println("Raio do cone: " + raioCone + " cm");
-        System.out.println("Altura do cone: " + alturaCone + " cm");
+        System.out.println("Altura do cone: " + alturaCone + " cm"); //printamos o resultado
      }
 
 }
