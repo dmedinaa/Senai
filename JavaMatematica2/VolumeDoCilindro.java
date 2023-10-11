f44d5fab0292e89846edae2d9a92a34a4ee0c93c
Danilo Medina <141359607+dmedinaa@users.noreply.github.com>
2023-10-11 14:01:40 -0300

    Update VolumeDoCilindro.java

--- JavaMatematica2/VolumeDoCilindro.java
+++ JavaMatematica2/VolumeDoCilindro.java
@@ -2,9 +2,10 @@ package JavaMatematica2;
 
 public class VolumeDoCilindro {
     public static void main(String[] args) {
-        double volumeCilindro = 785; // cm³
-        double alturaCilindro = 13; // cm
-        double raioCilindro = Math.sqrt(volumeCilindro / (Math.PI * alturaCilindro));
+        double volumeCilindro = 785; // adicionamos o volume do cilindro em cm²
+        double alturaCilindro = 13; // adicionamos a altura do Cilindro em cm²
+
+        double raioCilindro = Math.sqrt(volumeCilindro / (Math.PI * alturaCilindro)); // 
         System.out.println("Raio do cilindro: " + raioCilindro + " cm");
         System.out.println("Altura do cilindro: " + alturaCilindro + " cm");
     }
