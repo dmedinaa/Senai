bd140b182a0bb7c8319865a40e20f78e7cf547e1
Danilo Medina <141359607+dmedinaa@users.noreply.github.com>
2023-10-11 14:12:01 -0300

    Acrescentando comentário Piramide.java

--- JavaMatematica1/Piramide.java
+++ JavaMatematica1/Piramide.java
@@ -4,8 +4,9 @@ public class Piramide {
     public static void main(String[] args) {
         double ladoBase = 6;
         double altura = 8;
-        double volume = (1.0 / 3) * Math.pow(ladoBase, 2) * altura;
-        System.out.println("O volume da pirâmide é: " + volume);
+
+        double volume = (1.0 / 3) * Math.pow(ladoBase, 2) * altura; // Colocamos a formula e usamos o Math.pow calcular a potência.
+        System.out.println("O volume da pirâmide é: " + volume); // imprimimos o resultado.
     }
 
 }
