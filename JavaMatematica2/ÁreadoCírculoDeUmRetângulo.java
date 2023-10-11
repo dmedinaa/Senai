ee120b0edf3642ef86045a38844307b8c674e03a
Danilo Medina <141359607+dmedinaa@users.noreply.github.com>
2023-10-11 14:32:07 -0300

    Acrescentando comentário

--- JavaMatematica2/ÁreadoCírculoDeUmRetângulo.java
+++ JavaMatematica2/ÁreadoCírculoDeUmRetângulo.java
@@ -2,11 +2,15 @@ package JavaMatematica2;
 
 public class ÁreadoCírculoDeUmRetângulo {
     public static void main(String[] args) {
-        double comprimentoRetangulo = 20; // cm
-        double larguraRetangulo = 10; // cm
+        double comprimentoRetangulo = 20;
+        double larguraRetangulo = 10;
+        
         double perimetroCirculo = 2 * Math.PI * Math.sqrt((comprimentoRetangulo * larguraRetangulo) / Math.PI);
+
         double raioCirculo = perimetroCirculo / (2 * Math.PI);
+
         double areaCirculo = Math.PI * raioCirculo * raioCirculo;
+
         System.out.println("Raio do círculo: " + raioCirculo + " cm");
         System.out.println("Área do círculo: " + areaCirculo + " cm²");
     }
