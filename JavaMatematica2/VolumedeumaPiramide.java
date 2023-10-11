3c1501358663feae17a66b907f318bb5bfcd1fea
Danilo Medina <141359607+dmedinaa@users.noreply.github.com>
2023-10-11 14:31:48 -0300

    Acrescentando comentário

--- JavaMatematica2/VolumedeumaPiramide.java
+++ JavaMatematica2/VolumedeumaPiramide.java
@@ -2,10 +2,13 @@ package JavaMatematica2;
 
 public class VolumedeumaPiramide {
     public static void main(String[] args) {
-        double volumePiramide = 180; // cm³
-        double alturaPiramide = 10; // cm
+        double volumePiramide = 180; 
+        double alturaPiramide = 10; 
+
         double areaBasePiramide = volumePiramide / alturaPiramide; // formula para acharmos a area da piramide.
+
         double ladoBasePiramide = Math.sqrt(areaBasePiramide); // formula para acharmos o lado da  piramide.
+
         System.out.println("Lado da base da pirâmide: " + ladoBasePiramide + " cm");
         System.out.println("Altura da pirâmide: " + alturaPiramide + " cm"); // printamos os resultados.
     }
