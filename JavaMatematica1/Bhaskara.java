ff6116d86e2dd6acf2dec8a634884d16d82462d3
Danilo Medina <141359607+dmedinaa@users.noreply.github.com>
2023-10-11 14:08:02 -0300

    Acrescentando comentários Bhaskara.java

--- JavaMatematica1/Bhaskara.java
+++ JavaMatematica1/Bhaskara.java
@@ -7,7 +7,7 @@ public class Bhaskara {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Digite os coeficientes da equação quadrática:"); // criamos uma sequencia de scanner para salvar os dados fornecidos pelo usuário
+        System.out.println("Digite os coeficientes da equação quadrática:"); // criamos uma sequencia de scanner para salvar os dados fornecidos pelo usuário.
         System.out.print("a: ");
         double a = scanner.nextDouble();
         System.out.print("b: ");
@@ -15,9 +15,9 @@ public class Bhaskara {
         System.out.print("c: ");
         double c = scanner.nextDouble();
 
-        double delta = b * b - 4 * a * c; //colocamos a formula de delta
+        double delta = b * b - 4 * a * c; //colocamos a formula de delta.
 
-        if (delta > 0) { // if/esle para as contições de ser uma raiz real ou não
+        if (delta > 0) { // if/esle para as contições de ser uma raiz real ou não.
             double x1 = (-b + Math.sqrt(delta)) / (2 * a);
             double x2 = (-b - Math.sqrt(delta)) / (2 * a);
             System.out.println("As raízes da equação são: x1 = " + x1 + " e x2 = " + x2);
@@ -28,6 +28,6 @@ public class Bhaskara {
             System.out.println("A equação não possui raízes reais.");
         }
 
-        scanner.close();
+        scanner.close(); // fechamos o scanner para evitando exceções de esgotamento de recursos e outros problemas.
     }
 }
