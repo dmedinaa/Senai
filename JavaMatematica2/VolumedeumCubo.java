fa658972b87a5ca846d4229074943ef4a68d1a47
Danilo Medina <141359607+dmedinaa@users.noreply.github.com>
2023-10-11 14:29:31 -0300

    Acrescentando comentário VolumedeumCubo.java

--- JavaMatematica2/VolumedeumCubo.java
+++ JavaMatematica2/VolumedeumCubo.java
@@ -2,7 +2,7 @@ package JavaMatematica2;
 
 public class VolumedeumCubo {
     public static void main(String[] args) {
-        double volumeCubo = 512; // cm³
+        double volumeCubo = 512; 
         double arestaCubo = Math.cbrt(volumeCubo); // colocamos a biblioteca Math.cbrt para multiplicamos ao cubo ( x3).
         System.out.println("Comprimento da aresta do cubo: " + arestaCubo + " cm"); //printamos os resultados
         
