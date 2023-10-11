9e57b1d9efe329f4c2fb47ab33e49296f5209f5b
Danilo Medina <141359607+dmedinaa@users.noreply.github.com>
2023-10-11 14:17:23 -0300

    Acrescentando comentário AreaCirculo.java

--- JavaMatematica1/AreaCirculo.java
+++ JavaMatematica1/AreaCirculo.java
@@ -3,7 +3,9 @@ package JavaMatematica1;
 public class AreaCirculo {
     public static void main(String[] args) {
         double raio = 5;
-        double area = Math.PI * raio * raio;
+
+        double area = Math.PI * raio * raio; //adicionamos a formula 
+
         System.out.println("A área do círculo é: " + area);
  }
 
