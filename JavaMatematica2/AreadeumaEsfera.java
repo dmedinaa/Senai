0ca4324588ed87c02572d8780c766de420e57e0a
Danilo Medina <141359607+dmedinaa@users.noreply.github.com>
2023-10-11 14:28:17 -0300

    Acrescentando comentário AreadeumaEsfera.java

--- JavaMatematica2/AreadeumaEsfera.java
+++ JavaMatematica2/AreadeumaEsfera.java
@@ -3,8 +3,8 @@ package JavaMatematica2;
 public class AreadeumaEsfera {
     public static void main(String[] args) {
         double areaEsfera = 154; // cm²
-        double raioEsfera = Math.sqrt(areaEsfera / (4 * Math.PI));
-        System.out.println("Raio da esfera: " + raioEsfera + " cm");
+        double raioEsfera = Math.sqrt(areaEsfera / (4 * Math.PI)); //adicionamos a formula.
+        System.out.println("Raio da esfera: " + raioEsfera + " cm");//printamos os resultados.
     }
 
 }
