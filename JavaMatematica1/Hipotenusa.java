23bbd79de398157fd09aff6a007cce97cde53405
Danilo Medina <141359607+dmedinaa@users.noreply.github.com>
2023-10-11 14:09:18 -0300

    Update Hipotenusa.java

--- JavaMatematica1/Hipotenusa.java
+++ JavaMatematica1/Hipotenusa.java
@@ -4,7 +4,9 @@ public class Hipotenusa {
     public static void main(String[] args) {
         double a = 3;
         double b = 4;
-        double hipotenusa = Math.sqrt(a * a + b * b);
+
+        double hipotenusa = Math.sqrt(a * a + b * b); // adicionamos a formula da hipotenusa.
+
         System.out.println("A hipotenusa do triângulo é: " + hipotenusa);
     }
 }
