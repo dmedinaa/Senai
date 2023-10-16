2697a57fd70b6cba6384d379bf6723d20633ce01
Danilo Medina <141359607+dmedinaa@users.noreply.github.com>
2023-10-16 19:14:33 -0300

    Add files via upload

---
+++ QuimicaJava/MaximoDeEletrons.java
@@ -0,0 +1,9 @@
+package QuimicaJava;
+
+public class NumeroMaximoDeEletrons  {
+    public static void main(String[] args) {
+        int maxEletronsTerceiraCamada = 2 * (int) Math.pow(3, 2);
+        System.out.println("A terceira camada pode acomodar até " + maxEletronsTerceiraCamada + " elétrons.");
+    }
+}
+
