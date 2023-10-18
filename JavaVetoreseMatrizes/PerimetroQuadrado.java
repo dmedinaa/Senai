f6d2d8f64085eedc6d50bec404edb5c5da282b82
Danilo Medina <141359607+dmedinaa@users.noreply.github.com>
2023-10-18 20:03:14 -0300

    adição de comentários

--- JavaVetoreseMatrizes/PerimetroQuadrado.java
+++ JavaVetoreseMatrizes/PerimetroQuadrado.java
@@ -4,13 +4,17 @@ import java.util.Scanner;
 public class PerimetroQuadrado {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Digite o comprimento do lado do quadrado: ");
+        
+        System.out.print("Digite o comprimento do lado do quadrado: ");// Solicita e lê a entrada do usuário
         int lado = scanner.nextInt();
-        int perimetro = calcularPerimetroQuadrado(lado);
+        
+        
+        int perimetro = calcularPerimetroQuadrado(lado);// Calcula o perímetro do quadrado usando a fórmula e imprime o resultado
         System.out.println("Perímetro do quadrado: " + perimetro);
     }
 
-    public static int calcularPerimetroQuadrado(int lado) {
+   
+    public static int calcularPerimetroQuadrado(int lado) { // Função para calcular o perímetro do quadrado
         return 4 * lado;
     }
 }
