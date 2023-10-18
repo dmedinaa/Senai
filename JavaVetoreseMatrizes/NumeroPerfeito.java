4507c13577a60d5f2ea5b0cfa8f32fab10e57e99
Danilo Medina <141359607+dmedinaa@users.noreply.github.com>
2023-10-18 20:07:06 -0300

    adição de comentários

--- JavaVetoreseMatrizes/NumeroPerfeito.java
+++ JavaVetoreseMatrizes/NumeroPerfeito.java
@@ -3,9 +3,12 @@ import java.util.Scanner;
 
 public class NumeroPerfeito {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);// Solicita e lê a entrada do usuário
+        
         System.out.print("Digite um número: ");
         int numero = scanner.nextInt();
+        
+        // Verifica se o número é perfeito e imprime o resultado
         if (verificarNumeroPerfeito(numero)) {
             System.out.println("O número é perfeito.");
         } else {
@@ -13,6 +16,7 @@ public class NumeroPerfeito {
         }
     }
 
+    // Função para verificar se o número é perfeito
     public static boolean verificarNumeroPerfeito(int numero) {
         int somaDivisores = 0;
         for (int i = 1; i < numero; i++) {
