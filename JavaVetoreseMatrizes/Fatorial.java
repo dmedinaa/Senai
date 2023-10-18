ebf77c4614821eba72386013f53177609d7852e5
Danilo Medina <141359607+dmedinaa@users.noreply.github.com>
2023-10-18 20:05:34 -0300

    adição de comentários

--- JavaVetoreseMatrizes/Fatorial.java
+++ JavaVetoreseMatrizes/Fatorial.java
@@ -3,13 +3,17 @@ import java.util.Scanner;
 
 public class Fatorial {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);// Solicita e lê a entrada do usuário
+        
         System.out.print("Digite um número: ");
         int numero = scanner.nextInt();
+        
+        // Calcula o fatorial usando um loop e imprime o resultado
         int fatorial = calcularFatorial(numero);
         System.out.println("Fatorial de " + numero + ": " + fatorial);
     }
 
+    // Função para calcular o fatorial de um número
     public static int calcularFatorial(int numero) {
         if (numero == 0 || numero == 1) {
             return 1;
