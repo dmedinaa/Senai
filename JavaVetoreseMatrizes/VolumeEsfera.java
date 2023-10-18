a0659584e8a253e69729d0a551a824016959238e
Danilo Medina <141359607+dmedinaa@users.noreply.github.com>
2023-10-18 20:06:13 -0300

    adição de comentários

--- JavaVetoreseMatrizes/VolumeEsfera.java
+++ JavaVetoreseMatrizes/VolumeEsfera.java
@@ -3,14 +3,18 @@ import java.util.Scanner;
 
 public class VolumeEsfera {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in); // Solicita e lê a entrada do usuário
+       
         System.out.print("Digite o raio da esfera: ");
         double raio = scanner.nextDouble();
-        double volume = calcularVolumeEsfera(raio);
+        
+      
+        double volume = calcularVolumeEsfera(raio);  // Calcula o volume da esfera usando a fórmula e imprime o resultado
         System.out.println("Volume da esfera: " + volume);
     }
 
-    public static double calcularVolumeEsfera(double raio) {
+    
+    public static double calcularVolumeEsfera(double raio) {// Função para calcular o volume da esfera
         return (4.0 / 3.0) * Math.PI * Math.pow(raio, 3);
     }
 }
