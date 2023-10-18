e410d225da186826cf2162ee261dae960beeb0f8
Danilo Medina <141359607+dmedinaa@users.noreply.github.com>
2023-10-18 20:01:33 -0300

    adição de comentários

--- JavaVetoreseMatrizes/DeterminanteMatriz.java
+++ JavaVetoreseMatrizes/DeterminanteMatriz.java
@@ -2,12 +2,15 @@ package JavaVetoreseMatrizes;
 
 public class DeterminanteMatriz {
     public static void main(String[] args) {
+        
         int[][] matriz = {{2, 3}, {4, 1}};
+      
         int determinante = calcularDeterminante(matriz);
-        System.out.println("Determinante: " + determinante);
+        System.out.println("Determinante: " + determinante);  // Calcula usando a fórmula para matrizes 2x2 e imprime o resultado
     }
 
-    public static int calcularDeterminante(int[][] matriz) {
+    
+    public static int calcularDeterminante(int[][] matriz) {// Função para calcular o determinante de uma matriz 2x2
         return matriz[0][0] * matriz[1][1] - matriz[0][1] * matriz[1][0];
     }
 }
