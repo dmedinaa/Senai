f4af22c28edfa1936cd6d202b9e2212ec3c9507c
Danilo Medina <141359607+dmedinaa@users.noreply.github.com>
2023-10-18 20:04:08 -0300

    adição de comentarios

--- JavaVetoreseMatrizes/SomaDigitos.java
+++ JavaVetoreseMatrizes/SomaDigitos.java
@@ -3,14 +3,18 @@ import java.util.Scanner;
 
 public class SomaDigitos {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);// Solicita e lê a entrada do usuário
+        
         System.out.print("Digite um número: ");
         int numero = scanner.nextInt();
+        
+        // Calcula a soma dos dígitos usando um loop e imprime o resultado
         int soma = calcularSomaDigitos(numero);
         System.out.println("Soma dos dígitos: " + soma);
     }
 
-    public static int calcularSomaDigitos(int numero) {
+    
+    public static int calcularSomaDigitos(int numero) {// Função para calcular a soma dos dígitos de um número
         int soma = 0;
         while (numero != 0) {
             soma += numero % 10;
