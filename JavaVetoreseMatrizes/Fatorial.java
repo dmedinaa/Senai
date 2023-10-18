5f21db0e574a61eb20bdcdb035153ac82d96c506
Danilo Medina <141359607+dmedinaa@users.noreply.github.com>
2023-10-18 19:56:41 -0300

    Create Fatorial.java

---
+++ JavaVetoreseMatrizes/Fatorial.java
@@ -0,0 +1,23 @@
+package JavaVetoreseMatrizes;
+import java.util.Scanner;
+
+public class Fatorial {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite um número: ");
+        int numero = scanner.nextInt();
+        int fatorial = calcularFatorial(numero);
+        System.out.println("Fatorial de " + numero + ": " + fatorial);
+    }
+
+    public static int calcularFatorial(int numero) {
+        if (numero == 0 || numero == 1) {
+            return 1;
+        }
+        int resultado = 1;
+        for (int i = 2; i <= numero; i++) {
+            resultado *= i;
+        }
+        return resultado;
+    }
+}
